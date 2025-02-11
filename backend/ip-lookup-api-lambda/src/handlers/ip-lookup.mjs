13c49804d2c6f74ae214ca65f7eae7c04f53240e
Hasitha Gamage <hasitha.hpmax@gmail.com>
2025-02-11 15:29:22 +0800

    refactor: Simplify IP lookup handler and remove unnecessary comments

--- backend/ip-lookup-api-lambda/src/handlers/ip-lookup.mjs
+++ backend/ip-lookup-api-lambda/src/handlers/ip-lookup.mjs
@@ -1,15 +1,10 @@
-
-/**
- * A simple example includes a HTTP get method to get one item by id from a DynamoDB table.
- */
 export const getByIdHandler = async (event) => {
   if (event.httpMethod !== 'GET') {
     throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
   }
-  // All log statements are written to CloudWatch
+
   console.info('received:', event);
 
-  // Get id from pathParameters from APIGateway because of `/{id}` at template.yaml
   const ip = event.pathParameters.ip;
 
   const ipData = {
@@ -21,7 +16,6 @@ export const getByIdHandler = async (event) => {
     body: JSON.stringify(ipData)
   };
 
-  // All log statements are written to CloudWatch
   console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
   return response;
 }
