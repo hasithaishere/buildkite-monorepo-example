404561b380f665223279e8824497498a7a0cd538
Hasitha Gamage <hasitha.hpmax@gmail.com>
2025-02-12 03:21:05 +0800

    refactor: Simplify Buildkite pipeline input block generation

--- infra/cloudformation/frontend-host/.buildkite/scripts/generate-pipeline.sh
+++ infra/cloudformation/frontend-host/.buildkite/scripts/generate-pipeline.sh
@@ -27,7 +27,7 @@ get_parameters() {
     PARAMETERS=$(echo "$JSON_OUTPUT" | jq -c '.Parameters')
 
     # Initialize Buildkite pipeline YAML
-    PIPELINE_YAML="steps:\n  - block: \":earth_americas: Select Environment and AWS Region\"\n  input:\n      fields:"
+    PIPELINE_YAML="steps:\n  - input:\n      fields:"
 
     # Loop through parameters and generate fields based on Type
     FIELDS=$(echo "$PARAMETERS" | jq -r '
